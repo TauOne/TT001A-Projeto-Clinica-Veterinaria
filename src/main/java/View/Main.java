6750cfb02723eebd95b351123f836ed29c2da557
Tauan Rodrigues <Tauan.rod@gmail.com>
2022-10-17 20:39:01 -0300

    Criada a primeira JPanel

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -5,7 +5,12 @@
 
 package View;
 
+import Model.Animal;
 import Model.AnimalDAO;
+import Model.Cliente;
+import Model.ClienteDAO;
+import Model.Especie;
+import Model.EspecieDAO;
 
 
 /**
@@ -15,6 +20,13 @@ import Model.AnimalDAO;
 public class Main {
 
     public static void main(String[] args) {
-
+        //EspecieDAO.getInstance().create("Pokemon");
+        //ClienteDAO.getInstance().create("Tauan", "Sei lá qualquer rua", "0000000", "1546328", "Qualquercoisa@");
+        //AnimalDAO.getInstance().update(AnimalDAO.getInstance().retrieveById(1));
+        
+        //System.out.println(EspecieDAO.getInstance().retrieveAll());
+        //System.out.println(ClienteDAO.getInstance().retrieveAll());
+        //System.out.println(AnimalDAO.getInstance().retrieveAll());
+        
     }
 }
